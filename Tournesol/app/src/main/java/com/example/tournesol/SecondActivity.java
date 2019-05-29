2a40f5ecfaf71471fd794bc7794d775b83b45224
leo-hardy <40430728+leo-hardy@users.noreply.github.com>
2019-05-29 11:50:13 +0200

    add android

---
+++ Tournesol/app/src/main/java/com/example/tournesol/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.tournesol;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void onClick(View v){
+
+    }
+
+    public void tounerGauche(View v){}
+
+    public void tounerDroite(View v){}
+
+
+}
+
